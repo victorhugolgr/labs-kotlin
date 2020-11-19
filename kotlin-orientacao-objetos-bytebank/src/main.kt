45c78c1cc67ebb8fe5ffeebe1ad8c586308de3e6
victor.hugo <victor.hugo@tre-pa.jus.br>
2020-11-19 12:06:07 -0300

    10 - Restrições das variáveis

--- kotlin-orientacao-objetos-bytebank/src/main.kt
+++ kotlin-orientacao-objetos-bytebank/src/main.kt
@@ -1,7 +1,12 @@
 fun main(){
     println("Olá mundo!")
     val titular = "Victor"
-
+    val numeroConta = 10
+    var saldo = 0.0
+    saldo = 100.0 + 200
+    saldo += 1000
 
     println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
 }
