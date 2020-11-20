523e5065afc9d6f2f9c412f421c9f7aade0a7e25
victor.hugo <victor.hugo@tre-pa.jus.br>
2020-11-20 12:36:56 -0300

    Criando classes e objetos

--- kotlin-orientacao-objetos-bytebank/src/main.kt
+++ kotlin-orientacao-objetos-bytebank/src/main.kt
@@ -1,6 +1,31 @@
 fun main() {
     println("Olá mundo!")
 
+    val conta = Conta();
+    conta.titular = "Victor";
+
+    println(conta.titular);
+
+    val contaJaque = Conta();
+    contaJaque.titular = "Jaque";
+    println(contaJaque.titular);
+}
+
+class Conta {
+    var titular = "";
+    var numero = 0;
+    var saldo = 0.0;
+}
+
+fun testaCondicoes(saldo: Double) {
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é neutra")
+        else -> println("Conta é negativa")
+    }
+}
+
+fun testaLacos(){
     for (i in 5 downTo 1 step 2) {
 
         if(i==4){
@@ -14,15 +39,5 @@ fun main() {
         println("número da conta $numeroConta")
         println("saldo da conta $saldo")
         println("")
-    }
-
-    // testaCondicoes(saldo)
-}
-
-fun testaCondicoes(saldo: Double) {
-    when {
-        saldo > 0.0 -> println("Conta é positiva")
-        saldo == 0.0 -> println("Conta é neutra")
-        else -> println("Conta é negativa")
     }
 }
