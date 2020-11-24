1a099b28ecbf6ea4a325db7ebdf31db2232dbc69
victor.hugo <victor.hugo@tre-pa.jus.br>
2020-11-24 12:52:06 -0300

    Criando uma nova classe

--- kotlin-orientacao-objetos-bytebank/src/main.kt
+++ kotlin-orientacao-objetos-bytebank/src/main.kt
@@ -1,8 +1,14 @@
 fun main() {
-    println("Olá mundo!")
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "1111111",
+        salario = 1_000.0
+    )
 
-
-
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salrio ${alex.salario}")
+    println("bonificação ${alex.bonificacao()}")
 }
 
 
