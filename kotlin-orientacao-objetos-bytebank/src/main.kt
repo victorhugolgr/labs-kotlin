097d5188dbae0774830fc85221f1490dbe129dba
victor.hugo <victor.hugo@tre-pa.jus.br>
2020-11-24 17:56:57 -0300

    Utilizando o polimorfismo

--- kotlin-orientacao-objetos-bytebank/src/main.kt
+++ kotlin-orientacao-objetos-bytebank/src/main.kt
@@ -1,15 +1,34 @@
 fun main() {
-    val alex = Gerente(
+    val victor = Gerente(
         nome = "Alex",
         cpf = "1111111",
         salario = 1_000.0,
         senha = 1
     )
 
-    println("nome ${alex.nome}")
-    println("cpf ${alex.cpf}")
-    println("salrio ${alex.salario}")
-    println("bonificação ${alex.bonificacao}")
+    val jaque = Funcionario(
+        nome = "Jaque",
+        cpf = "22222",
+        salario = 900.0
+    )
+
+    val roberto = Diretor(
+        nome = "Roberto",
+        senha = 1,
+        cpf = "33333",
+        salario = 3_000.0,
+        plr = 200
+    )
+
+
+
+
+    val calculadora = CalculadoraBonificacao()
+    calculadora.registra(victor)
+    calculadora.registra(jaque)
+    calculadora.registra(roberto)
+
+    println("Total de bonificação : ${calculadora.total}")
 }
 
 
